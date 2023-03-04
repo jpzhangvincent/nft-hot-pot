54078dfdd7a967a037acdaa2408d91140d38c81c
Furious George <bbud148@gmail.com>
2023-03-04 14:01:21 -0700

    Update hardhat.config.js to add scrollTestnet

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -12,7 +12,12 @@ module.exports = {
 		ETH_GOERLI: {
 			accounts: [`${process.env.PRIVATE_KEY}`],
 			url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-		}
+		},
+		    scrollTestnet: {
+		      	url: process.env.SCROLL_TESTNET_URL || "",
+		      	accounts:
+			process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
 	},
 	etherscan: {
 		apiKey: `${process.env.ETHERSCAN_API_KEY}`
